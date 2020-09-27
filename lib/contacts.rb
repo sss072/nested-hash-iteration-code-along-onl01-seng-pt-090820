05c1a5fcf6fa07615484217d9a44ae68569967f0
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-27 21:57:21 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,16 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if {|flavor| flavor == "strawberry"}
+        end
+      end
+    end
+  end
+  
 
 end
