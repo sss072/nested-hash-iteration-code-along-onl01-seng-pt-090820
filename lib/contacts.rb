786ec91debebc7b2b9edd913e42272f16b5a4216
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-27 21:54:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person == "Freddy Mercury"
       data.each do |attribute, value|
-        
+        if attribute == favorite_ice_cream_flavors
+          
 
 end
